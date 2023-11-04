78a6a40739c701f9979615441a5df02b0b81ffb4
Ali Can <alicnn38@gmail.com>
2023-11-04 17:58:54 +0300

    Add project files.

---
+++ UploadImage/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace UploadImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.btnUploadImage = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbxNickName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(411, 274);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnSelectImage.Location = new System.Drawing.Point(32, 347);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(417, 35);
+            this.btnSelectImage.TabIndex = 1;
+            this.btnSelectImage.Text = "Resim Sec";
+            this.btnSelectImage.UseVisualStyleBackColor = false;
+            this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click);
+            // 
+            // btnUploadImage
+            // 
+            this.btnUploadImage.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnUploadImage.Location = new System.Drawing.Point(342, 394);
+            this.btnUploadImage.Name = "btnUploadImage";
+            this.btnUploadImage.Size = new System.Drawing.Size(107, 32);
+            this.btnUploadImage.TabIndex = 2;
+            this.btnUploadImage.Text = "Resim Yukle";
+            this.btnUploadImage.UseVisualStyleBackColor = false;
+            this.btnUploadImage.Click += new System.EventHandler(this.btnUploadImage_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(32, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(417, 302);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Upload Image";
+            // 
+            // tbxNickName
+            // 
+            this.tbxNickName.Location = new System.Drawing.Point(32, 397);
+            this.tbxNickName.Name = "tbxNickName";
+            this.tbxNickName.PlaceholderText = "  @NickName";
+            this.tbxNickName.Size = new System.Drawing.Size(304, 29);
+            this.tbxNickName.TabIndex = 4;
+            this.tbxNickName.TextChanged += new System.EventHandler(this.tbxNickName_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(476, 508);
+            this.Controls.Add(this.tbxNickName);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnUploadImage);
+            this.Controls.Add(this.btnSelectImage);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Upload Image To SQL Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.Button btnUploadImage;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbxNickName;
+    }
+}
+
---
+++ UploadImage/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; // Dosya islemleri icin kutuphane
+using System.Data.SqlClient;
+
+
+namespace UploadImage
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-4M0OQRD\\SQLEXPRESS;Initial Catalog=UPLOAD_IMAGE;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        string imagePath;
+        string NickName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnUploadImage.Enabled = false;
+        }
+
+        private void btnSelectImage_Click(object sender, EventArgs e)
+        {
+            btnUploadImage.Enabled = true;
+
+            openFileDialog1.Title = "Upload Image";
+            openFileDialog1.Filter = "JPG Dosyalari(*.jpg)|*.jpg| PNG Dosyalari(*.png)|*.png| JPEG Dosyalari(*.jpeg)|*.jpeg| GIF Dosyalari(*.gif)|*.gif| TIF Dosyalari(*.tif)|*.tif";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                imagePath = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnUploadImage_Click(object sender, EventArgs e)
+        {   if(pictureBox1.Image != null && tbxNickName.Text != "")
+            {
+                FileStream fileStream = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                byte[] image = binaryReader.ReadBytes((int)fileStream.Length);
+                binaryReader.Close();
+                fileStream.Close();
+
+                connection.Open();
+                String date = Convert.ToString(DateTime.Now);
+                SqlCommand command = new SqlCommand("insert into Images(Images, NickName, Upload_Date) values (@images, @nickname, @simdi)", connection);
+                command.Parameters.Add("@images", SqlDbType.Image, image.Length).Value = image;
+                command.Parameters.AddWithValue("@simdi", DateTime.Parse(date));
+                command.Parameters.AddWithValue("@nickname", NickName);
+                command.ExecuteNonQuery();
+                connection.Close();
+
+                MessageBox.Show("Resim Yukleme islemi basariyla tamamlandi.", "Islem Tamamlandi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Lutfen resmin basariyla yuklendiginden veya @NickName alanini doldugunuzdan emin olun.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tbxNickName_TextChanged(object sender, EventArgs e)
+        {
+            NickName = "@" + tbxNickName.Text;
+        }
+    }
+}
