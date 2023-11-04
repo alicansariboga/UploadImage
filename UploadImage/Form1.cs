4941e25da3501faa2c1fdda03bdad9eb00ad68e5
Ali Can <alicnn38@gmail.com>
2023-11-04 19:17:08 +0300

    Some Code changes.

--- UploadImage/Form1.cs
+++ UploadImage/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO; // Dosya islemleri icin kutuphane
+using System.IO; // Library for the file processes
 using System.Data.SqlClient;
 
 
@@ -15,7 +15,7 @@ namespace UploadImage
 {
     public partial class Form1 : Form
     {
-        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-4M0OQRD\\SQLEXPRESS;Initial Catalog=UPLOAD_IMAGE;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        SqlConnection connection = new SqlConnection("Add Your SQL Path for Connection");
 
         string imagePath;
         string NickName;
